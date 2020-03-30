031ab994507754831db4b634f8bc0e0fe3eefec6
Fasih Ahmad Fakhri <fasihahmadfakhri@gmail.com>
2020-03-30 20:29:36 +0530

    Beautify Emotions constant in config file tools/emotion-detection/emotions/emotions.js

--- tools/emotion-detection/emotions/emotions.js
+++ tools/emotion-detection/emotions/emotions.js
@@ -1,4 +1,21 @@
 const Emotions = {
-	"Disgust": ['./emotions/Disgust/4EA47A6F-1FFF-41DE-B44F-4B4DF4BCF622.gif', './emotions/Disgust/42E789EB-5660-48A7-B669-5F684C144480.gif', './emotions/Disgust/78DBBBF3-016F-4EFE-A91A-9EFC6C2C4D8B.gif', './emotions/Disgust/42C3D1E1-1C5E-4F4A-BA04-97699278EEE1.gif', './emotions/Disgust/40A5A8C3-C7FF-42B1-AF19-1A1390F9DB68.gif', './emotions/Disgust/47E7DDC7-FC0B-4B1F-A27A-D42828F24548.gif', './emotions/Disgust/8987F722-5513-4057-8B8D-A43952D63827.gif', './emotions/Disgust/E2B26D93-71CE-428D-A44E-06AA4B2CE017.gif', './emotions/Disgust/5C465FD3-A9D6-474E-A5EF-E705B7B9943F.gif', './emotions/Disgust/49BDED53-F83F-4C57-9D44-D09C0B502660.gif', './emotions/Disgust/C233B6C9-446E-4381-9B55-8F513F59DFCF.gif', './emotions/Disgust/79380E47-BD1F-42D8-822B-FDF2930297C1.gif'],
-	"Contempt": ['./emotions/Contempt/E2FC853B-BA2A-4936-8D3E-9CD571FFC788.gif', './emotions/Contempt/EAA9457C-778D-4F7E-B82E-571BF50480EB.gif']
+	"Disgust": [
+		'./emotions/Disgust/4EA47A6F-1FFF-41DE-B44F-4B4DF4BCF622.gif',
+		'./emotions/Disgust/42E789EB-5660-48A7-B669-5F684C144480.gif',
+		'./emotions/Disgust/78DBBBF3-016F-4EFE-A91A-9EFC6C2C4D8B.gif',
+		'./emotions/Disgust/42C3D1E1-1C5E-4F4A-BA04-97699278EEE1.gif',
+		'./emotions/Disgust/40A5A8C3-C7FF-42B1-AF19-1A1390F9DB68.gif',
+		'./emotions/Disgust/47E7DDC7-FC0B-4B1F-A27A-D42828F24548.gif',
+		'./emotions/Disgust/8987F722-5513-4057-8B8D-A43952D63827.gif',
+		'./emotions/Disgust/E2B26D93-71CE-428D-A44E-06AA4B2CE017.gif',
+		'./emotions/Disgust/5C465FD3-A9D6-474E-A5EF-E705B7B9943F.gif',
+		'./emotions/Disgust/49BDED53-F83F-4C57-9D44-D09C0B502660.gif',
+		'./emotions/Disgust/C233B6C9-446E-4381-9B55-8F513F59DFCF.gif',
+		'./emotions/Disgust/79380E47-BD1F-42D8-822B-FDF2930297C1.gif',
+	],
+
+	"Contempt": [
+		'./emotions/Contempt/E2FC853B-BA2A-4936-8D3E-9CD571FFC788.gif',
+		'./emotions/Contempt/EAA9457C-778D-4F7E-B82E-571BF50480EB.gif',
+	]
 }
